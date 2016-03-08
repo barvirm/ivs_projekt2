bd59357169f87c03cfeb30fca2e02eca66171fce
Marek Barvíř <barvirm@seznam.cz>
2016-03-08 14:41:53 +0100

    mat lib

---
+++ my_math.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    return a+b
+def odd(a,b):
+    return a-b
+def sqrt(a,n):
+    return a**(1/n)
+def pow(a,n):
+    return a**n
+def factorial(n):
+    for i in range(n):
+        n += n*n
+    return n
