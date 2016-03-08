dea8f6d1f2f66086cdf931007e9f1c40ee97de52
Marek Barvíř <barvirm@seznam.cz>
2016-03-08 17:13:21 +0100

    s

--- plot.py
+++ plot.py
@@ -1,1 +1,15 @@
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+from math import *
+
+' sin(i)*2'
+def plot(string):
+    x = np.array(np.arange(-10,10,0.01))
+    y = []
+    for i in x:
+        y.append(eval(string))
+    y = np.array(y)
+    p = plt.plot(x,y)
+    plt.show()
+
+plot('cos(i)*2')
