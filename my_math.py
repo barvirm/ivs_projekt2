440b12c397727f65d0ac01abfdc6cb92fed654ff
Marek Barvíř <barvirm@seznam.cz>
2016-03-08 16:00:36 +0100

    math

--- my_math.py
+++ my_math.py
@@ -1,12 +1,17 @@
 def add(a,b):
+    """ return a + b """
     return a+b
 def odd(a,b):
+    """ return a - b """
     return a-b
 def sqrt(a,n):
+    """ return sqrt of a """
     return a**(1/n)
 def pow(a,n):
+    """ return pow of a """
     return a**n
 def factorial(n):
+    """ return factorial of number n """
     for i in range(n):
         n += n*n
     return n
