3da3a6751fee5b0df8d352def0d6d8a8c72b949e
Marek Barvíř <barvirm@seznam.cz>
2016-03-09 11:42:05 +0100

    main update

--- main.py
+++ main.py
@@ -1,8 +1,53 @@
-import math
-import gui
-import plot
+import my_math
+import pygtk
+pygtk.require("2.0")
+
+import gtk
+import gtk.glade
+
+import numpy as np
+from matplotlib.figure as Figure
+from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
+import pylab
+
+pylab.hold(False)
 
 class Calculator():
-
     def __init__(self):
-        ## gui
+        self.x = []
+        self.y = []
+
+
+        self.gui_init()
+
+    def gui_init(self):
+        builder = gtk.Builder()
+        builder.add_from_file("main_gui.glade")
+        window = builder.get_object("main_window")
+
+        builder.connect_signals()
+        self.figure = Figure(figsize=(100,100), dpi=75 )
+        self.axis = self.figure.add_subplot(111)
+
+        self.canvas = FigureCanvas(self.figure)
+        self.canvas.show()
+
+        self.graphview = builder.get_object("PLOT")
+        self.graphview.pack_start(self.canvas,True,True)
+
+    ######################### SIGNALS #####################
+    def on_main_window_destroy(self, widget, data = None ):
+        gtk.main_quit()
+
+    #######################################################
+
+        def main(self):
+            self.window.show()
+            gtk.main()
+
+
+############################ Start app ####################
+
+if __name__ = '__main__':
+    app = Calculator()
+    app.main()
