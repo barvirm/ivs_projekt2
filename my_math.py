debbedb8b023f735d668fa036cf7baeff91c5413
Marek Barvíř <barvirm@seznam.cz>
2016-03-30 19:22:11 +0200

    test + math_lib

--- my_math.py
+++ my_math.py
@@ -9,18 +9,26 @@ def odd(a,b):
     return (a) - (b)
 def division(a,b):
     """ return a / b """
-    return float( (a) / (b) )
+    if (b == 0 ):
+        return None
+    return ( float(a) / float(b) )
 def multiplication(a,b):
     """ return a * b """
     return a*b
-def sqrt(a,n):
+def sqrt(a,n=2):
     """ return sqrt of a """
-    return a**(1/ (n) )
+    if (a < 0):
+        return None
+    return a**(1./ (n) )
 def pow(a,n):
     """ return pow of a """
     return (a)**(n)
 def factorial(n):
     """ return factorial of number n """
+    if (n <= 0):
+        return None
+    if ( isinstance(n,float) ):
+        return None
     res = 1
     for i in range(1,n+1):
         res = res * i
@@ -40,7 +48,7 @@ def cos(x):
 
     x = (x) % (2*pi)
 
-    for i in range(1,50):
+    for i in range(1,80):
         result += (-1.)**i * ( ( (x)**(2.*i) ) / factorial(2*i) )
     result += 1
     return result
@@ -59,29 +67,23 @@ def ln(x):
     if ( x <= 0):
         return None
     result = 0
-    if ( x < 1 ):
-        x = 1 - x
-        for i in range(1,50):
-            result -= x**i/i
-    else:
-        x = (x-1)/float(x+1)
-        n = 50
-        liche=2*n-1
-        while (n > 1 ):
-            n=n-1
-            result= (n**2 * x*x) / float( liche - result )
-            liche-=2
-        result = 2*x / (1-result)
+    x = (x-1)/float(x+1)
+    n = 250
+    liche=2*n-1
+    while (n > 1 ):
+        n=n-1
+        result= (n**2 * x*x) / float( liche - result )
+        liche-=2
+    result = 2*x / (1-result)
     return result
 
-def abs(x):
+def abx(x):
     if (x<0):
         return -(x)
     else:
         return x
 
 def modulo(a,n):
+    if (a < 0 ):
+        a=abx(a)
     return (a)%(n)
-
-
-print ln(3)
