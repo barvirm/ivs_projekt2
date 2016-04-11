b6ac9f0c7e2abc512591ca8844b002de0e6aa0c0
Petr Malaník <Malanik.Petr.gm@gmail.com>
2016-04-11 03:21:40 +0200

    Tests to Transformation of string
    Light version

--- test_string.py
+++ test_string.py
@@ -3,7 +3,20 @@
 #Made by Marek Barvir
 
 import unittest as ut
-import my_math as m
-## @class Testing mathematical library
+import transform_string as s
+## @class Testing string transform for eval
 class TestMyMath(ut.TestCase):
+	def test_modulo(self):
+		self.assertEqual(s.StrFce("2+5"),"2+5")
+		self.assertEqual(s.StrFce("2%5"),"modulo(2,5)")
+		self.assertEqual(s.StrFce("(2%5)"),"modulo(2,5)")
+		self.assertEqual(s.StrFce("2+5%2"),"2+modulo(5,2)")
+		self.assertEqual(s.StrFce("2+5%(2*2)"),"2+modulo(5,2*2)")
+		self.assertEqual(s.StrFce("2%3+5%2"),"modulo(2,3)+modulo(5,2)")
+		self.assertEqual(s.StrFce("(2%3)+(5%2)"),"(modulo(2,3))+(modulo(5,2))")
+		self.assertEqual(s.StrFce("(2%3**2)+5%2"),"(modulo(2,3**2))+modulo(5,2)")
 
+
+
+if ( __name__ == '__main__' ):
+    ut.main()
