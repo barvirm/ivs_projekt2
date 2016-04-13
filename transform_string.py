4148f93e88e12235551ee349eb39ed7db66ccb48
David Bábek <d.babek@seznam.cz>
2016-04-13 14:39:10 +0200

    Modulo

--- transform_string.py
+++ transform_string.py
@@ -125,6 +125,58 @@ def transform_abs(vstup):
         vstup = vstup[1:]
      
     return vstup
+def transform_modulo(vstup):
+    while "%" in vstup:
+        
+        i_centr = vstup.find("%")
+        i1 = i_centr-1
+    
+        while vstup[i1] >= '0' and vstup[i1] <= '9' or vstup[i1] == '.':
+            if i1 > 0:
+                i1 -= 1
+            else:
+                break
+
+        i2 = i_centr+1
+        while vstup[i2] >= '0' and vstup[i2] <= '9' or vstup[i2] == '.' or vstup[i2] == '-' or (vstup[i2] == '*' and (vstup[i2+1] == '*' or vstup[i2-1] == '*')):
+            if i2 > 0 :
+                i2 += 1
+                if i2 == len(vstup):
+                    break
+            else:
+                break
+
+
+        p1 = vstup[:i1+1]
+        p2 = vstup[i1+1:i_centr]
+        p3 = vstup[i_centr+1:i2]
+        p4 = vstup[i2:]
+        if p1.find('modulo(') != -1 and p1.endswith(')'):
+            p2 = p1
+            p1 = ""
+        if p4.find('modulo(') != -1 and p4.endswith(')'):
+            p3 = p4
+            p4 = ""
+        if p2 == "" and p1 != "":
+            p2 = p1
+            p1 = ""
+        if p3 == "" and p4 != "":
+            p3 = p4
+            p4 = ""
+
+        if p2 == "":
+            return False
+        if p3 == "0":
+            return False
+            
+        vstup =  p1 + "modulo(" + p2 + "," + p3 + ")" + p4
+
+        if vstup[0] == "+":
+            vstup = vstup[1:]
+
+
+    return vstup
+
 ## Transform entry input to form ready for eval function.
 # replace x! --> factorial(x)
 # replace a**b --> pow(a,b)
@@ -135,14 +187,13 @@ def StrFce(vstup):
     if type(vstup) == str:        
         # nahrazení řárek za tečku
         vstup = vstup.replace(",", ".")
-#        vstup = vstup.replace("(", "")
-#        vstup = vstup.replace(")", "")  
         #if vstup.find("m") !=0:
          #   print "sqrt"
         # factorial
         
         vstup=transform_factorial(vstup)
         vstup=transform_abs(vstup)        
+        vstup=transform_modulo(vstup)
         """
         back = 0
         frst = 0
@@ -166,66 +217,8 @@ def StrFce(vstup):
         if (absolut%2) != 0:
             return False
         """
-       
-        while "%" in vstup:
-        
-            i_centr = vstup.find("%")
-            i1 = i_centr-1
-    
-            while vstup[i1] >= '0' and vstup[i1] <= '9' or vstup[i1] == '.':
-                if i1 > 0:
-                    i1 -= 1
-                else:
-                    break
 
-            i2 = i_centr+1
-            while vstup[i2] >= '0' and vstup[i2] <= '9' or vstup[i2] == '.' or vstup[i2] == '-' or (vstup[i2] == '*' and (vstup[i2+1] == '*' or vstup[i2-1] == '*')):
-                if i2 > 0 :
-                    i2 += 1
-                    if i2 == len(vstup):
-                        break
-                else:
-                    break
-
-
-            p1 = vstup[:i1+1]
-            p2 = vstup[i1+1:i_centr]
-            p3 = vstup[i_centr+1:i2]
-            p4 = vstup[i2:]
-            if p1.find('modulo(') != -1 and p1.endswith(')'):
-                p2 = p1
-                p1 = ""
-            if p4.find('modulo(') != -1 and p4.endswith(')'):
-                p3 = p4
-                p4 = ""
-            if p2 == "" and p1 != "":
-                p2 = p1
-                p1 = ""
-            if p3 == "" and p4 != "":
-                p3 = p4
-                p4 = ""
-#            if p2.startswith('(') and p2.endswith(')'):
-#                p2 = p2[+1:-1]
-#            if p3.startswith('(') and p3.endswith(')'):
-#                p3 = p3[+1:-1]
-#            print "p1:",p1
-#            print "p2:",p2
-#            print "p3:",p3
-#            print "p4:",p4
-
-            if p2 == "":
-                return False
-            if p3 == "0":
-                return False
-            
-            vstup =  p1 + "modulo(" + p2 + "," + p3 + ")" + p4
-
-            if vstup[0] == "+":
-                vstup = vstup[1:]
-
-
-        return vstup
-            
+        return vstup    
     else:
         print "špatná vstupní data"
         
