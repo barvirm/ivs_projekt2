196a1d8ccde1b923d47c01bcf8e40e7e338adc2e
David <d.babek@seznam.cz>
2016-04-24 21:32:07 +0200

    Created tmp/

---
+++ tmp/usr/share/Kalkulacka/plot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Nebude tu
+import numpy as np
+import matplotlib.pyplot as plt
+from math import *
+
+' sin(i)*2'
+def plot(string):
+    x = np.array(np.arange(-10,10,0.01))
+    y = []
+    for i in x:
+        y.append(eval(string))
+    y = np.array(y)
+    p = plt.plot(x,y)
+    plt.show()
+
+plot('cos(i)*2')
