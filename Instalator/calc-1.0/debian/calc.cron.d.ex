86d429d1836146dfd78f12d0e47e7d3fb2533f69
Marek Barvir <barvirm@seznam.cz>
2016-04-25 14:28:40 +0200

    why ??

--- Instalator/calc-1.0/debian/calc.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the calc package
-#
-0 4	* * *	root	[ -x /usr/bin/calc_maintenance ] && /usr/bin/calc_maintenance
--- Instalator/calc-1.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(calc):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="calc" command="/usr/bin/calc"
